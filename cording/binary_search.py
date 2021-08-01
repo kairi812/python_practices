1a1ebdaf03e9c9f930a5166014d3741db0f837f3
kairi812 <31227478+kairi812@users.noreply.github.com>
2021-08-01 11:53:14 +0900

    change variables and add comments

--- cording/binary_search.py
+++ cording/binary_search.py
@@ -6,21 +6,21 @@ search_list = list(range(1,10001))
 obj = 60
 
 def binary_search(search_list, obj):
-    low = 0 #
-    high = len(search_list) - 1
+    left = 0 # most left position
+    right = len(search_list) - 1 # most right position
     count = 0
-    while low <= high:
-        mid = (low + high) // 2 # middle index
+    while left <= right:
+        mid = (left + right) // 2 # middle index
         print(mid)
         mid_value = search_list[mid] # middle value
         if obj < mid_value:
             # search value is more left 
-            high = mid - 1
+            right = mid - 1
             count += 1
             continue
         elif obj > mid_value:
             # search value is more right
-            low = mid + 1
+            left = mid + 1
             count += 1
             continue
 
